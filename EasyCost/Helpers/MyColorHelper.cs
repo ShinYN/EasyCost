36f0325af51d64c94af29cca1c95e05764dbb4b9
ShinYN <go1323@gmail.com>
2016-08-01 21:26:55 +0900

    Update data

---
+++ EasyCost/Helpers/MyColorHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace EasyCost.Helpers
+{
+    public static class MyColorHelper
+    {
+        public static SolidColorBrush GetSolidColorBrush(string hex)
+        {
+            hex = hex.Replace("#", string.Empty);
+            byte a = (byte)(Convert.ToUInt32(hex.Substring(0, 2), 16));
+            byte r = (byte)(Convert.ToUInt32(hex.Substring(2, 2), 16));
+            byte g = (byte)(Convert.ToUInt32(hex.Substring(4, 2), 16));
+            byte b = (byte)(Convert.ToUInt32(hex.Substring(6, 2), 16));
+
+            SolidColorBrush myBrush = new SolidColorBrush(Windows.UI.Color.FromArgb(a, r, g, b));
+            return myBrush;
+        }
+
+        public static Color GetSolidColor(string hex)
+        {
+            hex = hex.Replace("#", string.Empty);
+            byte a = (byte)(Convert.ToUInt32(hex.Substring(0, 2), 16));
+            byte r = (byte)(Convert.ToUInt32(hex.Substring(2, 2), 16));
+            byte g = (byte)(Convert.ToUInt32(hex.Substring(4, 2), 16));
+            byte b = (byte)(Convert.ToUInt32(hex.Substring(6, 2), 16));
+
+            return ColorHelper.FromArgb(a, r, g, b);
+        }
+    }
+}
--- EasyCost/Pages/StatisticsPage.xaml.cs
+++ EasyCost/Pages/StatisticsPage.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using EasyCost.Helpers;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +27,11 @@ namespace EasyCost.Pages
         public StatisticsPage()
         {
             this.InitializeComponent();
+        }
+
+        private void btnSearchDay_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            btnSearchDay.Background = new SolidColorBrush(MyColorHelper.GetSolidColor("#FF0000"));
         }
     }
 }
